537c415e35260e0c33959acc29910f383f60ec83
Maxim Gerashchenko <79747103+themaximus@users.noreply.github.com>
2022-04-04 15:15:51 +0300

    refactor:  redesign

--- src/button/selectCity.jsx
+++ src/button/selectCity.jsx
@@ -8,10 +8,10 @@ function Landing(props) {
   
     return (
       <div >
-        <div className={v.dw} >PIGAMA SHOP</div>
+        <div  className={v.dw} >PIJAMA SHOP</div>
         <h4 className='dw'>Выберите город:</h4>
-        <select className='dw'>
-      {props.state.map (state => { return <option key={state.id}> {state.name} {state.shop}</option>
+        <select className={v.dww}>
+      {props.state.map (state => { return <option className={v.dwww} key={state.id}> {state.name} {state.shop}</option>
   } )}
   </select>
       </div>
