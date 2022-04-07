aeac6b79f50b54b58d4607c169ce66525b186673
Maxim Gerashchenko <79747103+themaximus@users.noreply.github.com>
2022-04-07 20:05:56 +0300

    feat: feat

--- src/Components/AppB.js
+++ src/Components/AppB.js
@@ -2,6 +2,7 @@ import React from "react";
 import t from './AppB.module.css';
 import MenuAdmin from "./MenuAdmin";
 import Order from "./Order";
+import Product from "./Product";
 
 class AppB extends React.Component {
     render() {
@@ -9,10 +10,14 @@ class AppB extends React.Component {
             <div className={t.AppB}>
                 <div className={t.shopblock}>
                     <h1>Магаз</h1>
+
+                    <Product/>
+                    
+                    
                 </div>
 
            <Order/>
-           <MenuAdmin/>
+           <MenuAdmin jojo="cola" />
 
             </div>
         )
---
+++ src/Components/Product.jsx
@@ -0,0 +1,17 @@
+import c from './Product.module.css'
+
+
+function Product() {
+    return (
+        <div className={c.gg}>
+            <div className={c.Product} >
+            <img className={c.img} src="https://www.reserved.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/5/6/5671I-02X-010-1_6.jpg" />
+            </div>
+            <div className={c.nameProduct}>
+            <h1>Пижама для карликов </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
